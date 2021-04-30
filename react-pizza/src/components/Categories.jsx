a1e709f6a9a8aff9a5cb575d858e07af134b8982
igorplisko <igorplisko95@gmail.com>
2021-04-30 14:09:28 +0300

    com

--- react-pizza/src/components/Categories.jsx
+++ react-pizza/src/components/Categories.jsx
@@ -4,16 +4,19 @@ const Categories = ({ items, onClick }) => {
 
    const [activeItem, setActiveItem] = useState(null)
 
-   const onSelectItem = (index2) => {
-      setActiveItem(index2)
+
+   const onSelectItem = (i) => {
+      setActiveItem(i)
+
    }
 
    return (
       <div>
          <div className="categories">
             <ul>
-               <li>All</li>
-               {items.map((name, index1) => (
+
+               <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>All</li>
+               {items && items.map((name, index1) => (
                   <li className={activeItem === index1 ? 'active' : ''} onClick={() => onSelectItem(index1)} key={`${name}_${index1}`}>{name}</li>             //!  key={`${name}_${index}`}
 
                ))}
