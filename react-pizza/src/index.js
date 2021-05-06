e3d9029eb8c8480ff8be64dbfe5aa4f04dddcc15
igorplisko <igorplisko95@gmail.com>
2021-05-06 16:16:36 +0300

    commit

--- react-pizza/src/index.js
+++ react-pizza/src/index.js
@@ -2,31 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import store from './redux/store'
+import { Provider } from 'react-redux'
+
 
 import './scss/app.scss';
 import App from './App';
 
 
-const inc = () => {
-  store.dispatch({
-    type: 'ADD'
-  });
-}
-
-store.subscribe(() => {
-  console.log("changed ", store.getState())
-
-})
-
-
-// store.dispatch({ type: 'ADD' })
-// console.log(store)
-
 ReactDOM.render(
-
   <Router>
-    <button onClick={inc}>+1</button>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
   </Router>,
 
   document.getElementById('root')
